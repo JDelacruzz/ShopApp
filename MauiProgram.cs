6c896266eacd668c41e039134101d925d7675022
JDelacruzz <delacruzjuan07@hotmail.com>
2026-03-14 09:31:48 -0500

    Agregar archivos de proyecto.

---
+++ MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ShopApp.DataAcces;
+
+namespace ShopApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            var dbContext = new ShopDbContext();
+            dbContext.Database.EnsureCreated();
+            dbContext.Dispose();
+
+
+            builder.Services.AddSingleton<ShopDbContext>();
+
+
+
+
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.DataAcces;
+
+namespace ShopApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly ShopDbContext _dbContext;
+
+        public MainPage(ShopDbContext dbContext)
+        {
+            InitializeComponent();
+            _dbContext = dbContext;
+            CargarDatos();
+        }
+
+        private void CargarDatos()
+        {
+            var productosComputadoras = _dbContext.Products
+                .Include(p => p.Category)
+                .Where(p => p.Category.Nombre == "Computadoras")
+                .ToList();
+
+            var productosMayor1000 = _dbContext.Products
+                .Where(p => p.Precio > 1000)
+                .ToList();
+
+            var categoriaMasCara = _dbContext.Products
+                .Include(p => p.Category)
+                .OrderByDescending(p => p.Precio)
+                .Select(p => p.Category)
+                .FirstOrDefault();
+
+            lblComputadoras.Text = string.Join("\n", productosComputadoras
+                .Select(p => $"• {p.Nombre} - ${p.Precio}"));
+
+            lblMayor1000.Text = string.Join("\n", productosMayor1000
+                .Select(p => $"• {p.Nombre} - ${p.Precio}"));
+
+            lblCategoriaCara.Text = categoriaMasCara?.Nombre ?? "No encontrada";
+
+        }
+    }
+}
