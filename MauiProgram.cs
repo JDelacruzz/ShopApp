4f731bc30ae1f85c0db6d1dec35a51846b4bd101
JDelacruzz <delacruzjuan07@hotmail.com>
2026-03-20 23:33:03 -0500

    Se realizan cambios con el tab para mas comodidad en telefono

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -22,6 +22,7 @@ namespace ShopApp
             dbContext.Dispose();
 
 
+            Routing.RegisterRoute(nameof(ProductDetailPage), typeof(ProductDetailPage));
             builder.Services.AddSingleton<ShopDbContext>();
 
 
---
+++ Views/ClientsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShopApp.Views;
+
+public partial class ClientsPage : ContentView
+{
+	public ClientsPage()
+	{
+		InitializeComponent();
+	}
+}
